e90c185fe5bf84307855160e4a93c0cbc46450b9
Jeffrey Gamage <jeffrey.gamage@gmail.com>
2020-08-24 16:10:44 -0700

    Added IComparable string container class Implemented string list processor function

--- StringListProcessor/StringListProcessor/Program.cs
+++ StringListProcessor/StringListProcessor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StringListProcessor
 {
@@ -6,7 +7,38 @@ namespace StringListProcessor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> initial = new List<string>{ "one", "two", "three" };
+            List<string> to_add = new List<string> { "one", "two", "five","six" };
+            List<string> to_remove = new List<string> {"two", "five"};
+            List<string> result = ProcessStringLists(initial, to_add, to_remove);
+            foreach(string nextString in result)
+            {
+                Console.WriteLine(nextString);
+            }
+            Console.ReadLine();
+        }
+
+        static List<string> ProcessStringLists(List<string> initial, List<string> to_add, List<string> to_remove)
+        {
+            SortedSet<UrtheCastOrderedString> orderedStrings = new SortedSet<UrtheCastOrderedString>();
+            foreach(string nextString in initial)
+            {
+                orderedStrings.Add(new UrtheCastOrderedString(nextString));
+            }
+            foreach (string nextString in to_add)
+            {
+                orderedStrings.Add(new UrtheCastOrderedString(nextString));
+            }
+            List<string> result = new List<string>();
+            foreach(UrtheCastOrderedString orderedString in orderedStrings)
+            {
+                result.Add(orderedString._string);
+            }
+            foreach(string nextToRemove in to_remove)
+            {
+                result.Remove(nextToRemove);
+            }
+            return result;
         }
     }
 }
---
+++ StringListProcessor/StringListProcessor/UrtheCastOrderedString.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringListProcessor
+{
+    class UrtheCastOrderedString: IComparable
+    {
+        public string _string;
+
+        public UrtheCastOrderedString(string newString)
+        {
+            this._string = newString;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if(obj.GetType()!=typeof(UrtheCastOrderedString))
+            {
+                return 0;
+            }
+            if(((UrtheCastOrderedString)obj)._string.Length>_string.Length)
+            {
+                return 1;
+            }
+            if(((UrtheCastOrderedString)obj)._string.Length < _string.Length)
+            {
+                return -1;
+            }
+            for(int i=0;i<_string.Length;i++)
+            {
+                if(((UrtheCastOrderedString)obj)._string[i]!=_string[i])
+                {
+                    return ((UrtheCastOrderedString)obj)._string[i] - _string[i];
+                }
+            }
+            return 0;
+        }
+    }
+}
